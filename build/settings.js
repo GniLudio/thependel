629a14d9e27b5b39a369f53d957a624f69413a31
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-03-12 13:11:14 +0100

    Setup

---
+++ build/settings.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("settings.ts loaded");
+const wallpaperSettings = {
+    fps: 15
+};
---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+console.log("settings.ts loaded");
+
+const wallpaperSettings: ISettings = {
+    fps: 15
+}
+
+interface ISettings {
+    fps: number
+}
