cce1c53abc904a4470edc664cbfce142f9087447
GniLudio <ferdinand.rohlfing@googlemail.com>
2024-03-13 12:00:57 +0100

    Added types.d.ts and tree_generator.ts

---
+++ src/tree_generator.ts
@@ -0,0 +1,70 @@
+console.log("tree_generator.ts loaded");
+
+/**
+ * Generates the pendulum tree with n pendulums using the pruefer sequence.
+ * @param n The number of pendulums.
+ * @returns The root pendulum.
+ */
+function generatePendulumTree(n: number): Pendulum {
+    // creates the sequence
+    const sequence: number[] = [];
+    for (let i=0; i<n-2; i++) { sequence.push(randomInt(0, n-1)); }
+    // creates n pendulums (labeled from 0 to n-1)
+    const pendulums: Pendulum[] = [];
+    for (let i=0; i<n; i++) {pendulums[i] = new Pendulum();}
+    // create the list with 0 to n-1
+    const list: number[] = [];
+    for (let i=0; i<n; i++) {list.push(i);}
+
+    while (list.length > 2) {
+        // find the smallest number in the list which is not in the sequence
+        const s = Math.min(...list.filter(v => !sequence.includes(v)));
+        // deletes the first number from the list
+        list.splice(list.indexOf(s), 1);
+        // gets and deletes the first number from the sequence
+        const f = sequence.shift()!;
+        addEdge(s,f);
+    }
+    addEdge(list[0], list[1]);
+
+    makeTreeDirected(pendulums[0]);
+    //printTree(pendulums[0], pendulums);
+
+    return pendulums[0];
+
+    function addEdge(a: number, b: number): void {
+        pendulums[a].children.add(pendulums[b]);
+        pendulums[b].children.add(pendulums[a]);
+    }
+}
+
+/**
+ * Removes edges to parents.
+ * @param root 
+ */
+function makeTreeDirected(root: Pendulum): void {
+    for (const child of root.children) {
+        child.children.delete(root);
+        makeTreeDirected(child);
+    }
+}
+
+/**
+ * Prints the pendulum tree.
+ * @param root The root pendulum.
+ * @param indent The indentation level.
+ */
+function printTree(root: Pendulum, pendulums: Pendulum[], indent: string = ""): void {
+    console.log(indent + pendulums.indexOf(root));
+    root.children.forEach(child => printTree(child, pendulums, indent + "-"));
+}
+
+/**
+ * Generates a integer between a minimum and a maximum integer.
+ * @param min The minimum. (inclusive)
+ * @param max The maximum. (inclusive)
+ * @returns The random number.
+ */
+function randomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
---
+++ src/types.d.ts
@@ -0,0 +1,19 @@
+interface UserSettings {
+    pendulum_count: number,
+}
+
+interface GeneralSettings {
+    fps: number,
+}
+
+interface Window {
+    wallpaperPropertyListener: WallpaperListener;
+}
+
+interface WallpaperListener {
+    applyGeneralProperties?(properties: Partial<GeneralSettings>): void;
+    applyUserProperty?(properties: Partial<UserSettings>): void;
+    setPaused?(isPaused: boolean): void;
+    userDirectoryFilesAddedOrChanged?(propertyName: string, changedFiles: string[]): void;
+    userDirectoryFilesRemoved?(propertyName: string, removedFiles: string[]): void;
+}
